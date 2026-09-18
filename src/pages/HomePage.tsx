eecc1be3402e42c7fa2bfa5d28803b862b29a501
axelravelo <axel9ravelo@outlook.com>
2026-09-18 17:51:42 -0600

    Main structure for page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const HomePage = () => {
+    return(
+        <>
+            <Header />
+            <h1>This is the home page</h1>
+        </>
+    );
+}
+
+export default HomePage;
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const NotFoundPage = () => {
+    return(
+        <>
+            <Header />
+            <h1>404 Not Found</h1>
+        </>
+    );
+}
+
+export default NotFoundPage;
